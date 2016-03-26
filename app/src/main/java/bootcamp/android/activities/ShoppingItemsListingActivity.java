9c59a0a440d13941b1885d7d9b95bb5e10a48902
Vidhi Thakrar <vidhitha@thoughtworks.com>
2016-03-26 12:23:20 +0530

    <Assignment Solution> Using a RecyclerView and GridLayoutManager to display product title and image in Grid.

--- app/src/main/java/bootcamp/android/activities/ShoppingItemsListingActivity.java
+++ app/src/main/java/bootcamp/android/activities/ShoppingItemsListingActivity.java
@@ -1,11 +1,9 @@
 package bootcamp.android.activities;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.GridView;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 import java.util.List;
 
@@ -14,32 +12,19 @@ import bootcamp.android.adapters.ShoppingItemsListAdapter;
 import bootcamp.android.models.Product;
 import bootcamp.android.repositories.ProductRepository;
 
-import static bootcamp.android.constants.Constants.DESCRIPTION_KEY;
-import static bootcamp.android.constants.Constants.DRAWABLE_KEY;
-import static bootcamp.android.constants.Constants.TITLE_KEY;
-
 public class ShoppingItemsListingActivity extends Activity {
 
-    private ProductRepository productRepository;
+  private ProductRepository productRepository;
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        productRepository = new ProductRepository();
-        List<Product> products  = productRepository.getProducts();
-        GridView gridView = (GridView) findViewById(R.id.grid_view);
-        gridView.setAdapter(new ShoppingItemsListAdapter(this, products));
-        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
-                Product product = (Product) adapterView.getAdapter().getItem(position);
-                Intent intent = new Intent(getApplicationContext(), ProductDetailsActivity.class);
-                intent.putExtra(TITLE_KEY, product.getTitle());
-                intent.putExtra(DESCRIPTION_KEY, product.getDescription());
-                intent.putExtra(DRAWABLE_KEY, product.getDrawable());
-                startActivity(intent);
-            }
-        });
-    }
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.main);
+    productRepository = new ProductRepository();
+    List<Product> products = productRepository.getProducts();
+    RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+    recyclerView.setLayoutManager(new GridLayoutManager(this, 3));
+    ShoppingItemsListAdapter productArrayAdapter = new ShoppingItemsListAdapter(products);
+    recyclerView.setAdapter(productArrayAdapter);
+  }
 }
