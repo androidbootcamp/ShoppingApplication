b5fd1f67e1bfcbb67a4594525358e0df81902b82
Abhinav Manchanda <abhinav.manchanda@gmail.com>
2016-04-01 05:23:56 +0530

    <Assignment Solution> Saved and restored instance state of Shopping List Activity.

--- app/src/main/java/bootcamp/android/activities/ShoppingItemsListingActivity.java
+++ app/src/main/java/bootcamp/android/activities/ShoppingItemsListingActivity.java
@@ -3,12 +3,10 @@ package bootcamp.android.activities;
 import android.app.Activity;
 import android.app.ProgressDialog;
 import android.os.Bundle;
-import android.os.StrictMode;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
 import java.util.ArrayList;
-import java.util.List;
 
 import androidplugins.Callback;
 import bootcamp.android.R;
@@ -16,38 +14,44 @@ import bootcamp.android.adapters.ShoppingItemsListAdapter;
 import bootcamp.android.models.Product;
 import bootcamp.android.repositories.ProductRepository;
 
+import static bootcamp.android.constants.Constants.PRODUCTS_KEY;
+
 public class ShoppingItemsListingActivity extends Activity {
 
-  private List<Product> products;
+  private ArrayList<Product> products;
 
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.main);
-
-    StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-    StrictMode.setThreadPolicy(policy);
-
-    ProgressDialog progressDialog = ProgressDialog.show(this, "", "Loading...", true, true);
-
     RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
 
-    ProductRepository productRepository = new ProductRepository();
+    if(savedInstanceState == null || savedInstanceState.getParcelableArrayList(PRODUCTS_KEY) == null) {
+      final ProgressDialog progressDialog = ProgressDialog.show(this, "", "Loading...", true, true);
+      ProductRepository productRepository = new ProductRepository();
+      productRepository.getProducts(productsCallback(recyclerView, progressDialog));
+    } else {
+      this.products = savedInstanceState.getParcelableArrayList(PRODUCTS_KEY);
+      renderProducts(recyclerView, products);
+    }
+  }
 
-    productRepository.getProducts(productsCallback(recyclerView, progressDialog));
 
+  @Override
+  protected void onSaveInstanceState(Bundle outState) {
+    super.onSaveInstanceState(outState);
+    if(products != null) outState.putParcelableArrayList(PRODUCTS_KEY, products);
   }
 
   private Callback<ArrayList<Product>> productsCallback(final RecyclerView recyclerView, final ProgressDialog progressDialog) {
     return new Callback<ArrayList<Product>>() {
       public void execute(ArrayList<Product> products) {
+        ShoppingItemsListingActivity.this.products = products;
         renderProducts(recyclerView, products);
         progressDialog.dismiss();
       }
     };
   }
-
-
   private void renderProducts(RecyclerView recyclerView, ArrayList<Product> products) {
     recyclerView.setAdapter(new ShoppingItemsListAdapter(products));
     recyclerView.setLayoutManager(new GridLayoutManager(this, 3));
